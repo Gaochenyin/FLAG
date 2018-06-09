672c4d76b494a1d3215156bc5c63f90ce307e1a2
phdsky <huid.peng@gmail.com>
2018-06-09 12:19:51 +0800

    Reviewed logistic regression code

--- MLANG/II_Logistic_Regression/costFunctionReg.m
+++ MLANG/II_Logistic_Regression/costFunctionReg.m
@@ -17,9 +17,12 @@ regularizedTheta = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-J = 1 / m * (-y' * log(sigmoid(X * theta)) - (1 - y') * log(1 - sigmoid(X * theta))) + lambda / (2 * m) * (sum(theta.^2) - sum(theta(1,:).^2));
+J = 1 / m * (-y' * log(sigmoid(X * theta))  - ...
+    (1 - y') * log(1 - sigmoid(X * theta))) + ...
+    lambda / (2 * m) * (sum(theta.^2) - sum(theta(1, :).^2));
+    % Hint: You should not regularize theta(1).
 
-regularizedTheta(2:size(theta)) = lambda / m * theta(2:size(theta),:);
+regularizedTheta(2 : size(theta)) = lambda / m * theta(2:size(theta), :);
 
 grad = 1 / m * (X' * (sigmoid(X * theta) - y)) + regularizedTheta;
 
--- MLANG/II_Logistic_Regression/predict.m
+++ MLANG/II_Logistic_Regression/predict.m
@@ -15,13 +15,17 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-for i = 1 : m
-    if sigmoid(X(i,:) * theta) >= 0.5
-        p(i) = 1;
-    else
-        p(i) = 0;
-    end
-end
+% For loop version
+% for i = 1 : m
+%     if sigmoid(X(i, :) * theta) >= 0.5
+%         p(i) = 1;
+%     else
+%         p(i) = 0;
+%     end
+% end
+
+% Vectorize version
+p = (sigmoid(X * theta) >= 0.5);
  
 % =========================================================================
 
