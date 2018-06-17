9012a4ed825ad6657947d50186a63e4fcb761d10
phd <huid.peng@gmail.com>
2018-06-17 11:02:15 +0800

    Finished multi-classification and neural networks code of machine learning

---
+++ MLANG/III_Multi-Classification_NeuralNetworks/oneVsAll.m
@@ -0,0 +1,64 @@
+function [all_theta] = oneVsAll(X, y, num_labels, lambda)
+%ONEVSALL trains multiple logistic regression classifiers and returns all
+%the classifiers in a matrix all_theta, where the i-th row of all_theta 
+%corresponds to the classifier for label i
+%   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
+%   logisitc regression classifiers and returns each of these classifiers
+%   in a matrix all_theta, where the i-th row of all_theta corresponds 
+%   to the classifier for label i
+
+% Some useful variables
+m = size(X, 1);
+n = size(X, 2);
+
+% You need to return the following variables correctly 
+all_theta = zeros(num_labels, n + 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should complete the following code to train num_labels
+%               logistic regression classifiers with regularization
+%               parameter lambda. 
+%
+% Hint: theta(:) will return a column vector.
+%
+% Hint: You can use y == c to obtain a vector of 1's and 0's that tell use 
+%       whether the ground truth is true/false for this class.
+%
+% Note: For this assignment, we recommend using fmincg to optimize the cost
+%       function. It is okay to use a for-loop (for c = 1:num_labels) to
+%       loop over the different classes.
+%
+%       fmincg works similarly to fminunc, but is more efficient when we
+%       are dealing with large number of parameters.
+%
+% Example Code for fmincg:
+% 
+%     % Set Initial theta
+%     initial_theta = zeros(n + 1, 1);
+%     
+%     % Set options for fminunc
+%     options = optimset('GradObj', 'on', 'MaxIter', 50);
+% 
+%     % Run fmincg to obtain the optimal theta
+%     % This function will return theta and the cost 
+%     [theta] = ...
+%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+%                 initial_theta, options);
+
+for k = 1 : num_labels
+    initial_theta = zeros(n + 1,1);
+    
+    options = optimset('GradObj','on','MaxIter',50);
+    
+    [theta] = fmincg(@(t)(lrCostFunction(t,X,(y == k),lambda)),initial_theta,options); % 每次计算一个样本，确定一个分类器
+    % y 5000 * 1     k 1 : 10  ???  从X中提取10个类 （X包含5000个1 - 10字符的样本
+    % y为样本对应的字符 范围为1 - 10） 该函数确定对应识别概率最大（具有最大可能识别为这个字符）的样本值
+    all_theta(k,:) = theta'; 
+end
+
+% =========================================================================
+
+end
