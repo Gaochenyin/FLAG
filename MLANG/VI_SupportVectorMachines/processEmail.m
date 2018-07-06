74ac91788b6fa3565615dab6d4c48beeade2db9e
phdsky <huid.peng@gmail.com>
2018-07-06 22:05:23 +0800

    Reviewed svm spam code of VI

--- MLANG/VI_SupportVectorMachines/processEmail.m
+++ MLANG/VI_SupportVectorMachines/processEmail.m
@@ -57,9 +57,7 @@ l = 0;
 while ~isempty(email_contents)
 
     % Tokenize and also get rid of any punctuation
-    [str, email_contents] = ...
-       strtok(email_contents, ...
-              [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+    [str, email_contents] = strtok(email_contents, [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
    
     % Remove any non alphanumeric characters
     str = regexprep(str, '[^a-zA-Z0-9]', '');
@@ -97,16 +95,14 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
     
-    for idx = 1 :length(vocabList)
-        if (strcmp(str,vocabList{idx}) == 1)
-            word_indices = [word_indices ; idx];
+    for idx = 1 : length(vocabList)
+        if (strcmp(str, vocabList{idx}) == 1)
+            word_indices = [word_indices; idx];
             break;
         end
     end
     
     % =============================================================
-
-
     % Print to screen, ensuring that the output lines are not too long
     if (l + length(str) + 1) > 78
         fprintf('\n');
