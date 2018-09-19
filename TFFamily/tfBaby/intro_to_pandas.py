5caea54a363ce2561a7215286b9e4a152a61da85
phdsky <huid.peng@gmail.com>
2018-09-19 23:30:55 +0800

    pandas going on

--- TFFamily/tfBaby/intro_to_pandas.py
+++ TFFamily/tfBaby/intro_to_pandas.py
@@ -3,6 +3,13 @@ from __future__ import print_function
 import pandas as pd
 print(pd.__version__)
 
+pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
 
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
 
+pd.DataFrame({'City name': city_names, 'Population': population})
+
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe.describe()
 
