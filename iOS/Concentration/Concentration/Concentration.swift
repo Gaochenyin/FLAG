1883f947f379c5a6d25ba20f2e5818d49de23266
phdsky <huid.peng@gmail.com>
2018-09-26 23:15:34 +0800

    Simple MVC programming model

--- iOS/Concentration/Concentration/Concentration.swift
+++ iOS/Concentration/Concentration/Concentration.swift
@@ -21,7 +21,29 @@ class Concentration {
     var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMached {
+            // 1. card is not matched
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // 2. the choosen card is not we have choosen before
+                if cards[matchIndex].indentifier == cards[index].indentifier {
+                    cards[matchIndex].isMached = true
+                    cards[index].isMached = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }
+            // 2. if the card was choosen before or no choosen card
+            else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+        // 1. if the card was matched before, do nothing
     }
     
     // TODO: Shuffle the cards
