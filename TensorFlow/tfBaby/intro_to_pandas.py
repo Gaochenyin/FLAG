a51782a3af6a555c3136a54f64142d023e907aea
phdsky <huid.peng@gmail.com>
2018-09-28 23:39:49 +0800

    Add offline training dataset

--- TensorFlow/tfBaby/intro_to_pandas.py
+++ TensorFlow/tfBaby/intro_to_pandas.py
@@ -12,7 +12,7 @@ population = pd.Series([852469, 1015785, 485199])
 
 pd.DataFrame({'City name': city_names, 'Population': population})
 
-california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe = pd.read_csv("california_housing_train.csv", sep=",")
 print(california_housing_dataframe.describe())
 
 print(california_housing_dataframe.head())
