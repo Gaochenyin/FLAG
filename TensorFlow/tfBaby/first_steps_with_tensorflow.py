0c82c8bb48cad79e20168ef69a7dbb440e9539f6
phdsky <huid.peng@gmail.com>
2018-09-30 22:36:51 +0800

    TF first stepping...

--- TensorFlow/tfBaby/first_steps_with_tensorflow.py
+++ TensorFlow/tfBaby/first_steps_with_tensorflow.py
@@ -36,3 +36,14 @@ feature_columns = [tf.feature_column.numeric_column("total_rooms")]
 # Step 2
 # Define the label
 targets = california_housing_dataframe["median_house_value"]
+
+# Step 3
+# Use gradient descent as the optimizer for training the model
+my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+
+linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=my_optimizer)
+
